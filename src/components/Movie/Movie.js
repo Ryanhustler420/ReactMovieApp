14ec4a831a461b9b6ae1e066cae9a0b4b79339a6
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-05 01:22:43 +0530

    Scaffolding out the components for the movie page

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE} from '../../config';
+import Navigation from './../elements/Navigation/Navigation';
+import FourColGrid from './../elements/FourColGrid/FourColGrid';
+import Spinner from './../elements/Spinner/Spinner';
+import './Movie.css';
+import MovieInfo from './../elements/MovieInfo/MovieInfo';
+import MovieInfoBar from './../elements/MovieInfoBar/MovieInfoBar';
 
 class Movie extends Component {
     render() {
         return (
-            <div>
-                Movie Detail
+            <div className='rmdb-movie'>
+                <Navigation />
+                <MovieInfo />
+                <MovieInfoBar />
+                <FourColGrid />
+                <Spinner />
             </div>
         )
     }
