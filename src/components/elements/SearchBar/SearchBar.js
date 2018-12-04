794e60d104c29856822112b0addd5e4beef14178
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-04 23:19:31 +0530

    Scaffolding out the components for the home page

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                SearchBar
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
